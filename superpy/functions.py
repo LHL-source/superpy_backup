78fad3429f9e72c64ddf38cfcc2ad42e0ae78873
LHL-source <97887598+LHL-source@users.noreply.github.com>
2023-11-14 13:38:21 +0100

    Add files via upload

--- superpy/functions.py
+++ superpy/functions.py
@@ -4,9 +4,20 @@ from datetime import datetime
 from datetime import timedelta
 import csv
 import os
-
-
 import argparse
+
+
+#initiate 'current_day' and 'choosen_date_byUser'
+current_day=datetime.today()
+print('r11 current_day:',current_day)
+print('r12 type(current_day)', current_day)
+#choosen_date_byUser=current_day
+#print('14 choosen_date_byUser:',choosen_date_byUser)
+#print('r15 type(choosen_date_byUser)',choosen_date_byUser )
+
+#check if --advance_date is provided
+#if args.advance_time:
+     #choosen_date_byUser=advance_time(args.advance_time)
 
 def date_type(date_string,):#w?y
     try:
@@ -16,53 +27,51 @@ def date_type(date_string,):#w?y
     except ValueError:
         raise argparse.ArgumentTypeError("Invalid date format. Use 'year-month-day' (e.g.,'2023-10-15).")
 
-#hieronder test result of date_type w?? yes good job
-#result_date_type=date_type("2023-01-02")
-#print("In function.py:result_date_type",result_date_type )
 
-#current_day=date.today()#w?y
-#print(current_day)#w?y
-
-#
 def advance_time(advance_time):#w?yes
-    print('into function advance_time,advance_time: ',advance_time)#w?y
     current_day=date.today()
     future_date=current_day + timedelta(advance_time)
-    
-    print('future_date', future_date)
-
     return  future_date #w?y
 
+
 #advance_time(2)#w?y
-#test input:python main.py buy --product_name apple --price 0.5 --expiration_date 2023-4-2  #w?yes well done
 #define file_path
 file_path=os.path.join('data','bought.csv')
-def add_buy_product(product_name,buy_price,expiration_date):
+def add_buy_product(product_name,buy_date,buy_price,expiration_date,):
+  print('r 41 product_name',product_name)
+  print('r 42 buy_date',buy_date)# was buy_date.strftime('%Y-%m-%d')
+  print('r 41 buy_price ',buy_price)
+  print('r 41 expiration_date ',expiration_date)
+  
+  if buy_date is None:
+        buy_date = datetime.today()
+  
   with open (file_path,'r') as file:
     reader=csv.DictReader(file)
     rows=list(reader)
-    #print('r36 rows',rows)#w?y
+    
 
     maximum=0
-    for eachRow in rows:
-        #print(' r 48 eachRows',eachRow)#w?y 
-        print("eachRow['id']",eachRow['id'])#w?yes
+    for eachRow in rows:#works? y
         #de waarde/value van id is een string conver naar een integer 
-        id_int=int(eachRow['id'])
-        #print('id_int',id_int)#wy
-        #print('type(id_int)',type(id_int))#w?y
+        id_int=int(eachRow['id'])#works?y
         if id_int > maximum:
             maximum =id_int
-            #print('r48 maximum', maximum)# w? yes good job
     new_row_id=maximum + 1
     
-    current_day=date.today()
+    current_day=date.today()# niet nodig in test fase wel in productie fase
+    print('r 60 current_day:',current_day)
+    print('r 61 type(current_day):',type(current_day))
+    formated_currentday_object=current_day.strftime('%Y-%m-%d')
+    print('r61 formated_currentday_object:',formated_currentday_object)
+    print('r62 type(formated_currentday_object):',type(formated_currentday_object))
+    #test_day = datetime.strptime('2020-01-01', '%Y-%m-%d')
    
     
     new_row ={
     'id':new_row_id,
     'product_name':product_name,
-    'buy_date':date_type(current_day),
+    'buy_date':date_type(buy_date.strftime('%Y-%m-%d')),#date_type(formated_currentday_object),oude gegeven:date_type(buy_date.strftime('%Y-%m-%d'))
     'buy_price':buy_price,
     'expiration_date':expiration_date,
     
@@ -74,14 +83,12 @@ def add_buy_product(product_name,buy_price,expiration_date):
     # add to the file: bought.csv:test_file_with_Id.csv
 
     
-  #with open (file_path,'w', newline='') as file:#w?yes wel done
-      #writer=csv.DictWriter(file,fieldnames=reader.fieldnames)
-      #writer.writeheader()
-      #writer.writerows(rows)
+  with open (file_path,'w', newline='') as file:#w?yes wel done
+      writer=csv.DictWriter(file,fieldnames=reader.fieldnames)
+      writer.writeheader()
+      writer.writerows(rows)
   return
 
-#print_add_buy_product=add_buy_product('apple', '0.5', '2023-4-2')#works?yes
-#print('print_add_buy_product',print_add_buy_product)
 
 #here under the function for "add_sold_product"#w?
 sold_file_path=os.path.join('data', 'sold.csv')
@@ -95,85 +102,52 @@ def add_sold_product(product_name,sell_price):
       with open (sold_file_path,'r') as sold_file:#w?y
            sold_reader= csv.DictReader(sold_file)#w?y
            sold_rows=list(sold_reader)#w?y
-           #print('sold_rows',sold_rows)#w/y
-     #open bought.csv
-      #with open (file_path,'r')as bought_file:#w?y
-           #bought_reader=csv.DictReader(bought_file)#w?y
-           #bought_rows=list(bought_reader)#w?y
-           #print('bought_rows',bought_rows)#w/y
+           
         
            #step 2: made for sold.csv id which will cumulate automatically
            maximum = 0
            for each_sold_Row in sold_rows:#w?yes
-               #print('r104 each_sold_Row',each_sold_Row)#w?
-               #print only here under the VALUE of sold_Row_ (not the key)
-               #print(" r 106 value:",each_sold_Row['id'])
-               #print here under only the KEY with name: id of each_sold_Row
-               #print(" r 107 only the name of the key with name id:",'id')
                #convert sold id to integer
                sold_id_convert_to_integer=int(each_sold_Row['id'])
-               #print('r 112 sold_id_convert_to_integer',sold_id_convert_to_integer)#w?y
-               #print('r 113 sold_id_convert_to_integer',type(sold_id_convert_to_integer))#w?y
                if sold_id_convert_to_integer >  maximum:
                   maximum= sold_id_convert_to_integer#w?y
-                  #print('maximum',maximum)#w?y
-                  new_sold_row_id= maximum + 1#w?y
-           #print(' r 118 new_sold_row_id',new_sold_row_id)#w?y?
-      
+           new_sold_row_id= maximum + 1#w?y
+              
       #stap 3 :open the bought.csv file #w? yes 
       with open (bought_file_path, 'r') as bought_file:#w?y
            bought_reader=csv.DictReader(bought_file)
            bought_rows=list(bought_reader)
-           #print('bought_rows',bought_rows)#w?y
-
            #stap 3.1:put the input :product_name into a variabel
            product_name_1=product_name
-           #print('r 126 product_name_1:',product_name_1)#w?
            #step:3.2 make a minimum date
            expiration_date_min=datetime.max
-           #print('r 141  expiration_date_min',expiration_date_min)#w?y
            #stap 3.3:go through the hole bought.csv and make a selection of items only with product_name=appel
            appel_list=[]
            for eachItem_appel_list in bought_rows:
                if eachItem_appel_list ['product_name']==product_name_1:#w?t
-                   #print('r 132 product_name_1==product_name TRUE ')#wYes
                    appel_list.append(eachItem_appel_list)#w?y
-                   #print('r134 appel_list_add_item',appel_list)#w?y
-
                    #stap 3.4: loop met de for/loop door the appel_list en haalt op de laatste:expiratation_date
                    #date_str="2010-12-31"
                    date_format="%Y-%m-%d"
-                   
                    for eachItem_appel_list_1 in appel_list:
-                       #print("r142 eachItem_appel_list_1['expiration_date'] ",eachItem_appel_list_1['expiration_date'])#w?y
                        #print("r143 eachItem_appel_list_1['expiration_date']",type(eachItem_appel_list_1['expiration_date']) ) #let op type is string so convert into type datetime                  
                        eachItem_appel_list_1_convert=datetime.strptime(eachItem_appel_list_1['expiration_date'],date_format)
-                       #print('r 145 (waarde) eachItem_appel_list_1_convert:', eachItem_appel_list_1_convert)#w?y
-                       #print('r 146  type(eachItem_appel_list_1_convert):', type(eachItem_appel_list_1_convert))#w?y
                        if eachItem_appel_list_1_convert < expiration_date_min:
                             expiration_date_min=eachItem_appel_list_1_convert 
                             #step 3.5 find the buy_id and put in variabel:buy_id_min_expDate
                             buy_id_min_expDate= eachItem_appel_list_1['id']                     
                             print('157 expiration_date_min' ,expiration_date_min)#wy
                             print('158 buy_id_min_expDate:', buy_id_min_expDate)#w/y
-                       #print the expiration_date of the FIRST row of the list
-
-                       #print('expiration_date_min expiration_date_min)
-                       #if eachItem_appel_list_1 ['expiration_date']:
-                            print('r 163 id', new_sold_row_id) #4 w?y
-                            print('r 164 bought_id',buy_id_min_expDate)#3 w?y
-                            print(' r165 sell_date', current_day)#6-11-2023  w?y
-                            print('r 166 sell_price', sell_price)#2.0  w?y
+                      
                        # step 3.6 make a new row and append to sold.csv
-                   new_row={
+           new_row={
                           'id': new_sold_row_id,
                           'bought_id': buy_id_min_expDate,
                           'sell_date': current_day.strftime('%Y-%m-%d'),
                           'sell_price':sell_price
-                            }  
-                      
-                   sold_rows.append(new_row)#w?y
-                   print('r 176 rows ', sold_rows)#w?y good job
+                    }        
+           sold_rows.append(new_row)#w?y
+           print('r 176 rows ', sold_rows)#w?y good job
            #
       with open (sold_file_path,'w', newline='') as file:#w?yes wel done
                         writer=csv.DictWriter(file,fieldnames=sold_reader.fieldnames)
